d21ee371475c41c2d501df4f005e17403036abe2
WienWong <wangzichengfu@gmail.com>
2015-06-01 20:58:19 +0200

    Create Quiz_0.py

---
+++ Quizzes/Quiz_0.py
@@ -0,0 +1,32 @@
+
+# Quiz 0 
+
+# Question 1
+print 'She shouted "Hello!" very loudly.'
+print 'Hello, world.'
+print "This course is great!"
+print
+
+# Question 4
+print (8 + (1 + (2 * 4) - 3))
+print (7 - 2) / (3 ** 2)
+print 7 / +4
+print 8 / -2
+print 5 - 1 - 3 - 7 - 0
+print (8 + (1 + (2 * 4) - 3))
+print
+
+# Question 5
+ounces = 16
+print ounces
+print 
+
+# Question 7
+MYnumber=11
+print MYnumber
+my_number=12
+print my_number
+ounces=13
+print ounces
+__number__=14
+print __number__
